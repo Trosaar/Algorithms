f56e750eb475c89bb4ec9cfc06dd8c6b77f90ac8
Aaron T <troskaaaron@gmail.com>
2020-03-16 16:00:00 -0500

    finished stock optimzation

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,11 +6,38 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+
+	if cache == None:
+		cache = {
+			"total_ways": 0
+		}
+
+	if n in cache:
+		return cache[n]
+
+	if n >= 3:
+		cache["total_ways"] += 1
+		eating_cookies(n-3, cache)
+	if n >= 2:
+		cache["total_ways"] += 1
+		eating_cookies(n-2, cache)
+	if n >= 1:
+		cache["total_ways"] += 1
+		eating_cookies(n-1, cache)
+	if n == 0:
+		return 1
+	
+	if cache != None and n not in cache:
+		cache[n] = cache["total_ways"]
+
+	print(cache)
+	return cache["total_ways"]
+
+eating_cookies(10)
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+	if len(sys.argv) > 1:
+		num_cookies = int(sys.argv[1])
+		print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+	else:
+		print('Usage: eating_cookies.py [num_cookies]')
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,26 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+	lowest = 0
+	highest = 1
+	margin = 0
+
+	for i in range(1, len(prices)):
+		for j in range(i, len(prices)):
+			if -prices[lowest] + prices[j] > -prices[lowest] + prices[highest]:
+				highest = j
+				print("sell", j, prices[j])
+		if -prices[i] + prices[highest] > -prices[lowest] + prices[highest] and i < highest:
+			lowest = i
+			print("buy", i, prices[i])
+
+	return -prices[lowest] + prices[highest]
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+	# This is just some code to accept inputs from the command line
+	parser = argparse.ArgumentParser(description='Find max profit from prices.')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+	args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+	print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
